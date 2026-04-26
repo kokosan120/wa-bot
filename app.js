013edfbfc335ed5aaee2b29ec0fa6fd68590f159
kokosan120 <tinyji6887@gmail.com>
2026-04-26 19:48:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('MAG Bot is running!'));
-app.listen(process.env.PORT || 3000, () => console.log('Web server is ready!'));
+app.listen(process.env.PORT || 7860, () => console.log('Web server is ready!'));
 const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
